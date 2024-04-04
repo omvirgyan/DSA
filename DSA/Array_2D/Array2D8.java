0619117641e36168a418344233a84b5261f8e94d
omvirgyan <119617353+omvirgyan@users.noreply.github.com>
2024-04-04 23:32:22 +0530

    Multiply

---
+++ DSA/Array_2D/Array2D8.java
@@ -0,0 +1,48 @@
+public class Array2D8 {
+    public static void main(String[] args) {
+        int[][] array1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] array2 = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        int[][] result = multiplyArrays(array1, array2);
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] multiplyArrays(int[][] arr1, int[][] arr2) {
+        int rows1 = arr1.length;
+        int cols1 = arr1[0].length;
+        int rows2 = arr2.length;
+        int cols2 = arr2[0].length;
+
+        if (cols1 != rows2) {
+            System.out.println("Cannot multiply these matrices. Inner dimensions must match.");
+            return null;
+        }
+
+        int[][] result = new int[rows1][cols2];
+
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                for (int k = 0; k < cols1; k++) {
+                    result[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+}
