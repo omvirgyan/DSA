35d07d96c8af0147a9a5a703449bf38801116b8b
omvirgyan <119617353+omvirgyan@users.noreply.github.com>
2024-04-05 23:47:42 +0530

    Spiral print

---
+++ DSA/Array_2D/Array2D9.java
@@ -0,0 +1,56 @@
+package DSA.Array_2D;
+
+public class Array2D9 {
+    
+    public static void spiralPrint(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+
+        int topRow = 0;
+        int bottomRow = matrix.length - 1;
+        int leftCol = 0;
+        int rightCol = matrix[0].length - 1;
+
+        while (topRow <= bottomRow && leftCol <= rightCol) {
+            // Print top row
+            for (int i = leftCol; i <= rightCol; i++) {
+                System.out.print(matrix[topRow][i] + " ");
+            }
+            topRow++;
+
+            // Print right column
+            for (int i = topRow; i <= bottomRow; i++) {
+                System.out.print(matrix[i][rightCol] + " ");
+            }
+            rightCol--;
+
+            // Print bottom row
+            if (topRow <= bottomRow) {
+                for (int i = rightCol; i >= leftCol; i--) {
+                    System.out.print(matrix[bottomRow][i] + " ");
+                }
+                bottomRow--;
+            }
+
+            // Print left column
+            if (leftCol <= rightCol) {
+                for (int i = bottomRow; i >= topRow; i--) {
+                    System.out.print(matrix[i][leftCol] + " ");
+                }
+                leftCol++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        spiralPrint(matrix); // Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+    }
+}
